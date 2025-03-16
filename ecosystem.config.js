6a4d785ed28a6e0b3cac29f4daf181a4ff14d3fd
cybercyberz <cybercyberz@gmail.com>
2025-03-17 00:55:12 +1100

    fix: simplify PM2 configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,14 @@
 module.exports = {
   apps: [{
     name: 'websitecursor',
-    script: 'next',
-    cwd: '/home/flocaclo/public_html',
+    script: 'npm',
+    args: 'start',
     env: {
       NODE_ENV: 'production',
       PORT: 3000,
       NODE_OPTIONS: '--max_old_space_size=512'
     },
-    instances: 1,
-    exec_mode: 'fork',
-    max_memory_restart: '512M',
     watch: false,
-    autorestart: true,
-    kill_timeout: 3000
+    autorestart: true
   }]
 }; 
