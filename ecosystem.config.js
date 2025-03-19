4f6d3e4e97e9fb29b867a47bf68b60c82dbed6d6
cybercyberz <cybercyberz@gmail.com>
2025-03-19 12:57:57 +1100

    fix: update pm2 config to use npm start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,13 @@
 module.exports = {
   apps: [{
     name: 'websitecursor',
-    script: './node_modules/next/dist/bin/next',
+    script: 'npm',
     args: 'start',
     cwd: '/home/flocaclo/public_html',
     env: {
       NODE_ENV: 'production',
       PORT: 3000,
-      HOST: '0.0.0.0',
-      NODE_OPTIONS: '--max_old_space_size=512'
+      HOST: '127.0.0.1'
     },
     watch: false,
     autorestart: true,
