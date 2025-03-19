add2a75ee1a5e3f56d11606e6b8b3104b96a5230
cybercyberz <cybercyberz@gmail.com>
2025-03-19 14:10:24 +1100

    feat: update pm2 config to use start.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,1 @@
-module.exports = {
-  apps: [{
-    name: 'websitecursor',
-    script: './node_modules/next/dist/bin/next',
-    args: 'start',
-    cwd: '/home/flocaclo/public_html',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000,
-      HOST: '127.0.0.1'
-    },
-    watch: false,
-    autorestart: true,
-    max_memory_restart: '512M'
-  }]
-}; 
+module.exports = { apps: [{ name: 'websitecursor', script: 'start.js', cwd: '/home/flocaclo/public_html', env: { NODE_ENV: 'production', PORT: 3000 }, instances: 1, exec_mode: 'fork', watch: false, autorestart: true, max_memory_restart: '512M' }] };
