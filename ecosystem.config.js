0fbccdaa06a3df4d3732cf2e6ba071cdb1577873
cybercyberz <cybercyberz@gmail.com>
2025-03-21 13:19:14 +1100

    Update server.js and ecosystem.config.js to serve static files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'websitecursor',
-    script: '.next/standalone/server.js',
+    script: 'server.js',
     cwd: '/home/flocaclo/public_html',
     instances: 1,
     exec_mode: 'fork',
